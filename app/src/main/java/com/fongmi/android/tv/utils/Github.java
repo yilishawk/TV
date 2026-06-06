21419fe84929c616da9ffbafb258b3ad6af2e412
yilishawk <51566598+yilishawk@users.noreply.github.com>
2026-06-06 10:08:35 +0800

    Update Github.java
    Signed-off-by: yilishawk <51566598+yilishawk@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/utils/Github.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Github.java
@@ -2,7 +2,7 @@ package com.fongmi.android.tv.utils;
 
 public class Github {
 
-    public static final String URL = "https://raw.githubusercontent.com/FongMi/Release/fongmi";
+    public static final String URL = "https://raw.githubusercontent.com/yilishawk/TV/release";
 
     private static String getUrl(String name) {
         return URL + "/apk/" + name;
